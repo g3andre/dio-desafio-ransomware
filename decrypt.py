20e3ce7fc6e134e86e8fdd9ee94bf763206b4c29
Andre M. Matos <36033735+g3andre@users.noreply.github.com>
2023-05-08 10:48:51 -0300

    Create decrypt.py
    Adicionado arquivo para descriptografar

---
+++ decrypt.py
@@ -0,0 +1,26 @@
+import os
+import pyaes
+
+## Abre o arquivo que contem a chave de criptografia
+file_key = open('.key',"rb")
+key = file_key.read()
+file_key.close()
+
+## abrir o arquivo criptografado
+file_name = "teste.txt.ransomwaretroll"
+file = open(file_name, "rb")
+file_data = file.read()
+file.close()
+
+## chave para descriptografia
+aes = pyaes.AESModeOfOperationCTR(key)
+decrypt_data = aes.decrypt(file_data)
+
+## remover o arquivo criptografado
+os.remove(file_name)
+
+## criar o arquivo descriptografado
+new_file = "teste.txt"
+new_file = open(f'{new_file}', "wb")
+new_file.write(decrypt_data)
+new_file.close()
